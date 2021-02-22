158059f13b6990518b2f131a7b54d524574c3e7a
“ZjqDo” <13516611059@163.com>
2021-02-22 10:46:12 +0800

    添加二维码扫描

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import 'mint-ui/lib/style.css'
 
 //配置请求的路径
 // axios.defaults.baseURL='http://localhost:8081/api/'
-axios.defaults.baseURL='http://172.20.109.163:9000/Lesso/'
+axios.defaults.baseURL='https://172.20.109.163:19000/Lesso/'
 // 设置拦截器
 axios.interceptors.request.use(config => {
   // config.headers.Authorization=window.sessionStorage.getItem('token')
