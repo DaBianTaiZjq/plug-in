27860e03708141f14a7ed426d3504efe52aab412
“ZjqDo” <13516611059@163.com>
2021-02-24 11:30:24 +0800

    修改API端口

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ module.exports={
       // proxy:"http://172.20.109.163:9001/", //开发环境的跨域问题解决，后端springboot服务ip 和 端口
       // port: 8081 //dev模式下的前端服务端口，也就是浏览器通过localhost：8085 访问前端页面
       // 例如：http://172.20.109.163:9001/Login/Verify?account=manager&password=654321
-      proxy:{
-        '/api':{
-          target:'https://172.20.109.163:19000/Lesso/',
-          changeOrigin: true,
-          pathRewrite:{
-            '^/api':''
-          }
-        }
-      },
+      // proxy:{
+      //   '/api':{
+      //     target:'https://172.20.109.163:19000/Lesso/',
+      //     changeOrigin: true,
+      //     pathRewrite:{
+      //       '^/api':''
+      //     }
+      //   }
+      // },
       https: true
     },
     css: {
